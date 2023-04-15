786c94ce5179457b8207a3929bb2af063e7f31d4
sander <sandersmit.net@gmail.com>
2023-04-15 15:09:55 +0200

    commenting

--- js/modules/displayVoertuigDetail.js
+++ js/modules/displayVoertuigDetail.js
@@ -2,9 +2,9 @@ import {
     fetchEndpoint2
 } from './sourceData.js';
 
+
 function setVoertuigDetailPage(event) {
-    event.target.parentElement.classList.add('test');
-    ///setting data attribute to link on the detail page
+    ///setting data attribute to link on the detail page 
     const voertuiglinkAttr = event.target.parentElement.getAttribute("data-voertuigid");
     //setting local storage 
     setLocalStorage(voertuiglinkAttr);
@@ -15,18 +15,20 @@ function setLocalStorage(voertuiglinkAttr){
     localStorage.setItem(newId, voertuiglinkAttr);
 }
 
+//use local storage to fetch new endpoint
 function loadvoertuigDetailPage() {
     let lastestItem = localStorage.length;
     const currvoertuiglinkAttrLocalStorage = localStorage.getItem(lastestItem);
      fetchEndpoint2(currvoertuiglinkAttrLocalStorage);
 }
-
+//print the ui for the detail page
 function printpredefined(predefinedVoortuig, VoertuigDetailContainer) {
    const listholder = document.createElement("ul");
    const backBtn = document.createElement("a");
    const header = document.createElement("header");
    backBtn.classList.add("backlink");
    backBtn.setAttribute("href", "index.html");
+   //loop trough keys and values for detailpage data
     let generatedKeyArray = Object.keys(predefinedVoortuig[0]);
     let generatedValueArray = Object.values(predefinedVoortuig[0]);
       for (var i = 0; i < 3; i++) {
